008b406945219d9268e008fe277c86e028e801f5
Yuhao <yuhao2804@gmail.com>
2026-04-29 01:07:24 +0200

    Create __init__.py

---
+++ agents/__init__.py
@@ -0,0 +1,19 @@
+"""Agents package for the Autonomous Neuro-Research Pipeline."""
+
+from agents import (
+    agent1_proposer,
+    agent2_reviewer,
+    agent3_experiment,
+    agent4_validator,
+    agent5_developer,
+    agent6_writer,
+)
+
+__all__ = [
+    "agent1_proposer",
+    "agent2_reviewer",
+    "agent3_experiment",
+    "agent4_validator",
+    "agent5_developer",
+    "agent6_writer",
+]
